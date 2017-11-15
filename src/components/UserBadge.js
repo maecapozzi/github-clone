865491bc6b98e1062af9791c920aec68d0ed0f8a
maecapozzi <maecapozzi@gmail.com>
2017-11-15 16:14:49 -0500

    Finish styling UserBadge component

--- src/components/UserBadge.js
+++ src/components/UserBadge.js
@@ -3,13 +3,11 @@ import Card from 'material-ui/Card'
 import PropTypes from 'prop-types'
 
 const UserBadge = ({profileImage, name, numFollowers, numFollowing}) => (
-  <div className='grid-item full'>
-    <Card className='card'>
-      <img className='image__profile' src={profileImage} />
-      <h1>{name}</h1>
-      <p>Followers: {numFollowers}</p>
-      <p>Following: {numFollowing}</p>
-    </Card>
+  <div className='grid-item'>
+    <img className='image__profile' src={profileImage} />
+    <h1>{name}</h1>
+    <p>Followers: {numFollowers}</p>
+    <p>Following: {numFollowing}</p>
   </div>
 )
 
